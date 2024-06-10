cc7dba3a194443f3d0fcc758123d4f390cba2f99
aibunny <waihenyafredrick@gmail.com>
2024-06-10 19:58:15 +0300

    {refactor} fix typo

--- init/cron-backup.sh
+++ init/cron-backup.sh
@@ -1,8 +1,8 @@
 set -e
 
-# Set up cron job to run dbt every minute in the working directory
+# Set up cron job to run incremental backup every hour
 until
-    sudo echo "0 * * * * root pgbackrest --stanza=aibunny --log-level-console=info backup  >> /var/log/cron.log 2>&1" > /etc/cron.d/incr-backup-cron
+    sudo echo "0 * * * * root pgbackrest --stanza=aibunny --log-level-console=info --type=incr  >> /var/log/cron.log 2>&1" > /etc/cron.d/incr-backup-cron
     sudo chmod 0644 /etc/cron.d/incr-backup-cron
 do 
     echo "Setting up cron for incremental backup"
@@ -11,18 +11,12 @@ done
     echo 'cron for incremental backup has been set up'
 
 
-# Set up cron job to run dbt every minute in the working directory
+# Set up cron job to run full backup every 1 am in the morning on saturdays
 until
-    sudo echo "0 1 * * * root pgbackrest --stanza=aibunny --log-level-console=info --type=full backup  >> /var/log/cron.log 2>&1" > /etc/cron.d/full-backup-cron
+    sudo echo "0 1 * * 6 root pgbackrest --stanza=aibunny --log-level-console=info --type=full backup  >> /var/log/cron.log 2>&1" > /etc/cron.d/full-backup-cron
     sudo chmod 0644 /etc/cron.d/full-backup-cron
 do 
     echo "Setting up cron for full backup"
     sleep 1s
 done
     echo 'cron for full backup has been set up'
-
-
-
-
-
-
